cd30c0038622388c1999db880a4429ac1b200da2
Logan Paxton <111173907+LoganPaxton@users.noreply.github.com>
2024-05-09 13:34:13 -0400

    Added API key generation, and fixed sign in.

--- cli/main.py
+++ cli/main.py
@@ -14,6 +14,8 @@ client = supabase.create_client(url, key)
 # Generate Fernet key
 f = Fernet(Fernet.generate_key())
 
+# Initialize logged_in variable
+logged_in = False
 
 def gen_charset(char=""):
     charset = ""
@@ -32,16 +34,19 @@ def gen_api_key(charset=""):
 
 
 def handle_login(key=""):
-  response = client.from_('UUIDs').select('id').eq('id', key).execute()
-  if response.get('error'):
-    print("Error:", response['error'])
-    return None
-  data = response.get('data')
-  if data:
-    return data[0]['id']
-  return None
-
-
+    global logged_in  # Declare global variable
+    response = client.from_('UUIDs').select('id').eq('id', key).execute()
+    data = response.data
+    try:
+        assert data and len(data) > 0
+        print("Logged in successfully!")
+        logged_in = True  # Update logged_in to True after successful login
+        global uid
+        uid = data[0]['id']
+        return True  # Return True if login is successful
+    except AssertionError:
+        print("Invalid UUID. Please try again.")
+        return False  # Return False if login fails
 
 
 def handle_signup():
@@ -57,19 +62,47 @@ def handle_signup():
 print("Hello, welcome to the API key generator!")
 print("This will be used for SecureDB.")
 print("--Options--")
-print("1. Sign Up\n2. Sign In\n3. Generate API Key")
-opt = input(">> ")
-if opt == "1":
-    handle_signup()
-elif opt == "2":
-    print("Please enter your key")
-    ukey = input(">> ")
-    user_id = handle_login(ukey)
-    if user_id:
-        print("Login successful. User ID:", user_id)
+print("1. Sign Up\n2. Sign In\n3. Generate API Key\n4. Exit")
+
+
+while True:
+    opt = input(">> ")
+
+    if opt == "1":
+        handle_signup()
+    elif opt == "2":
+        print("Please enter your key")
+        ukey = input(">> ")
+        handle_login(ukey)
+    elif opt == "3":
+        if logged_in:
+            response = client.from_('UUIDs').select('api_key').eq('id', uid).execute()
+            data = response.data
+            if data and len(data) > 0:  # Check if API key already exists
+                api_key = data[0]['api_key']
+                if api_key:
+                    print("An API key has already been generated for you:", api_key)
+                else:
+                    print("No API key found for your account. Generating a new one...")
+                    charset = gen_charset()  # Generate charset
+                    api_key = gen_api_key(charset)  # Generate API key using charset
+                    data, count = client.table('UUIDs').update({
+                        "api_key": api_key
+                    }).eq('id', uid).execute()  # Update user's API key in the database
+                    print("API key generated successfully:", api_key)  # Print generated API key
+            else:
+                print("No API key found for your account. Generating a new one...")
+                charset = gen_charset()  # Generate charset
+                api_key = gen_api_key(charset)  # Generate API key using charset
+                data, count = client.table('UUIDs').update({
+                    "api_key": api_key
+                }).eq('id', uid).execute()  # Update user's API key in the database
+                print("API key generated successfully:", api_key)  # Print generated API key
+        else:
+            print("Please log in first!")  # Inform user to log in before generating API key
+    
+    elif opt == "4":
+        print("Goodbye!")
+        break
     else:
-        print("Invalid UUID. Please try again.")
-elif opt == "3":
-    print("Sorry! Generating an API Key is not available at this time!")
-else:
-    print("Please enter a valid option!")
+        print("Please enter a valid option!")
